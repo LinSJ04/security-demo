10d5041a07ea6894450b59b88906ec253668f455
林思佳 <2318988232@qq.com>
2025-06-27 14:28:09 +0800

    整合数据源

---
+++ src/main/java/com/example/securitydemo/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.example.securitydemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.securitydemo.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ src/main/java/com/example/securitydemo/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.securitydemo.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.securitydemo.entity.User;
+
+public interface UserService extends IService<User> {
+}
