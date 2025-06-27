f4d271c41615de59f86608e3ea4d840cb3a16201
林思佳 <2318988232@qq.com>
2025-06-27 14:57:07 +0800

    SpringSecurity默认配置

---
+++ src/main/java/com/example/securitydemo/config/DBUserDetailsManager.java
@@ -0,0 +1,79 @@
+package com.example.securitydemo.config;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.securitydemo.entity.User;
+import com.example.securitydemo.mapper.UserMapper;
+import jakarta.annotation.Resource;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+public class DBUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService {
+    
+    @Resource
+    private UserMapper userMapper;
+
+    /**
+     * 从数据库中获取用户信息
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username", username);
+        User user = userMapper.selectOne(queryWrapper);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        } else {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getEnabled(),
+                    true, //用户账号是否未过期
+                    true, //用户凭证是否未过期
+                    true, //用户是否未被锁定
+                    authorities); //权限列表
+        }
+    }
+
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void deleteUser(String username) {
+
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return false;
+    }
+}
--- src/main/java/com/example/securitydemo/config/WebSecurityConfig.java
+++ src/main/java/com/example/securitydemo/config/WebSecurityConfig.java
@@ -2,23 +2,44 @@ package com.example.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 // @EnableWebSecurity // 开启SpringSecurity的自定义配置(在SpringBoot项目中可省略)
 public class WebSecurityConfig {
 
+	// 默认配置(可省)
 	@Bean
-	public UserDetailsService userDetailsService() {
-		// 创建基于内存的用户信息管理器 自定义配置会替换默认配置(包括application.properties)
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		// 使用manager管理UserDetails对象
-		manager.createUser(
-				// 创建UserDetails对象，用于管理用户名、用户密码、用户角色、用户权限等内容
-				User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build());
-		return manager;
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		//authorizeRequests()：开启授权保护 用户认证
+		//anyRequest()：对所有请求开启授权保护
+		//authenticated()：已认证请求会自动被授权
+		http
+				// 开启授权保护
+				.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+				// 默认的登录表单和默认的登出页面
+				.formLogin(withDefaults());//表单授权方式
+				// 浏览器自带的登录框，没有登录表单，没有默认的登出页 一般不使用
+//				.httpBasic(withDefaults());//基本授权方式
+
+		return http.build();
 	}
+
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		// 创建基于内存的用户信息管理器 自定义配置会替换默认配置(包括application.properties)
+//		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//		// 使用manager管理UserDetails对象
+//		manager.createUser(
+//				// 创建UserDetails对象，用于管理用户名、用户密码、用户角色、用户权限等内容
+//				User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build());
+//		return manager;
+//	}
 }
