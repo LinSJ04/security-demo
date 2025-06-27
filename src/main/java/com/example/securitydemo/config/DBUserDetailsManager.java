eaf7472d2c45d4ddfc69ae28b1b7b42d6cd2fc26
林思佳 <2318988232@qq.com>
2025-06-27 15:00:26 +0800

    添加用户功能

--- src/main/java/com/example/securitydemo/config/DBUserDetailsManager.java
+++ src/main/java/com/example/securitydemo/config/DBUserDetailsManager.java
@@ -52,9 +52,18 @@ public class DBUserDetailsManager implements UserDetailsManager, UserDetailsPass
         return null;
     }
 
+    /**
+     * 在数据库中插入新的用户信息
+     * @param userDetails
+     */
     @Override
-    public void createUser(UserDetails user) {
+    public void createUser(UserDetails userDetails) {
 
+        User user = new User();
+        user.setUsername(userDetails.getUsername());
+        user.setPassword(userDetails.getPassword());
+        user.setEnabled(true);
+        userMapper.insert(user);
     }
 
     @Override
